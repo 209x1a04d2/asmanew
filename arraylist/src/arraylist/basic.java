4c62792d2cdc29c4ef987547aebdb27acd30669a
Asma <209x1a04d2@gprec.ac.in>
2023-01-04 07:13:30 +0530

    "MY COMMIT"

---
+++ arraylist/src/arraylist/basic.java
@@ -0,0 +1,22 @@
+package arraylist;
+
+import java.util.ArrayList;
+
+public class basic {
+
+	public static void main(String[] args) {
+		ArrayList<Integer> arr = new ArrayList<>();
+		arr.add(0);
+		arr.add(1);
+		arr.add(2);
+		arr.add(3);
+		arr.add(4);
+		for(int i = 0; i < arr.size() ;i++)
+			System.out.println(arr.get(i));
+		
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ arraylist/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author INCUBATION LAB
+ *
+ */
+module arraylist {
+}
